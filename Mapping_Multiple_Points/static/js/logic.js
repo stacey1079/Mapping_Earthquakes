50c543c0225e37156a629739fdbe5ae4a2816066
Stacey Marotta <staceydmarotta@gmail.com>
2023-02-23 18:31:41 -0500

    push to new branch

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -4,13 +4,6 @@ console.log("working");
 // Create the map object with a center and zoom level.
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
-//  Add a marker to the map for Los Angeles, California.
-// L.circleMarker([34.0522, -118.2437], {
-//    color: 'black',
-//   fillColor: '#ffffa1',
-//    radius: 300
-// }).addTo(map);
-
 // Get data from cities.js
 let cityData = cities;
 
